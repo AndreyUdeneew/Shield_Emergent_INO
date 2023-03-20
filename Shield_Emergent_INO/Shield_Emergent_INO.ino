d60fb98e8bc3264d40e8dc09ba01fac7d1de0efb
Andrey <andrey.udeneew@mail.ru>
2023-03-20 17:14:46 +0300

    v0.6 4 buttons were added

--- Shield_Emergent_INO/Shield_Emergent_INO.ino
+++ Shield_Emergent_INO/Shield_Emergent_INO.ino
@@ -7,9 +7,19 @@ uint8_t EN2 = 6;
 uint8_t FLT1 = 26;
 uint8_t FLT2 = 7;
 uint8_t faultChannel = 0;
+uint8_t buttonPressed = 0;
+uint8_t Button1 = 2;
+uint8_t Button2 = 8;
+uint8_t Button3 = 3;
+uint8_t Button4 = 9;
+uint8_t keyboardState = 0;
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(Button1, INPUT_PULLUP);// 4 Rpi Pico pull_up must be after the attachinterrupt. It's a bug.
+  pinMode(Button2, INPUT_PULLUP);// 4 Rpi Pico pull_up must be after the attachinterrupt. It's a bug.
+  pinMode(Button3, INPUT_PULLUP);// 4 Rpi Pico pull_up must be after the attachinterrupt. It's a bug.
+  pinMode(Button4, INPUT_PULLUP);// 4 Rpi Pico pull_up must be after the attachinterrupt. It's a bug.
   pinMode(EN1, OUTPUT);
   pinMode(EN2, OUTPUT);
   digitalWrite(EN1, HIGH);
@@ -18,11 +28,10 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(FLT2), Fault2, LOW);
   pinMode(FLT1, INPUT_PULLUP);// 4 Rpi Pico pull_up must be after the attachinterrupt. It's a bug.
   pinMode(FLT2, INPUT_PULLUP);// 4 Rpi Pico pull_up must be after the attachinterrupt. It's a bug.
+  
+
   Serial.begin(115200);
   Serial.setTimeout(100);
-//  while (!Serial) {
-//    ;
-//  }
 }
 
 void loop() {
@@ -41,6 +50,7 @@ void loop() {
     Serial.println("FLT2");
     faultChannel = 0;
   }
+  keyboardScan();
 }
 
 void Fault1()
@@ -50,6 +60,29 @@ void Fault1()
 void Fault2()
 {
   faultChannel = 2;
+}
+
+uint8_t keyboardScan()
+{
+  if(digitalRead(Button1) == LOW)
+  {
+    keyboardState |= 0b00000001;
+  }
+    if(digitalRead(Button2) == LOW)
+  {
+    keyboardState |= 0b00000010;
+  }
+    if(digitalRead(Button3) == LOW)
+  {
+    keyboardState |= 0b00000100;
+  }
+    if(digitalRead(Button4) == LOW)
+  {
+    keyboardState |= 0b00001000;
+  }
+  delay(150);
+  Serial.println(keyboardState);
+  keyboardState = 0;
 }
 
 void waiting_4_command() {
