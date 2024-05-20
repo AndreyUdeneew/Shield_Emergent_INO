196096aeec78cfe6de642badd0e5b241c4203de0
Andrey <andrey.udeneew@mail.ru>
2024-05-20 14:41:08 +0300

    v0.7 Ready 4 the new PCB without keyboard

--- Shield_Emergent_INO/Shield_Emergent_INO.ino
+++ Shield_Emergent_INO/Shield_Emergent_INO.ino
@@ -2,32 +2,32 @@
 // and "FLT2" in the case of the power falt of the scond camera
 // send "RES1", "RES2" or "RESB" to reset camera 1, camera 2 or both cameras respectively
 String cmd, num_of_pins_str, CMDcur;
-uint8_t EN1 = 27;
-uint8_t EN2 = 6;
-uint8_t FLT1 = 26;
-uint8_t FLT2 = 7;
+uint8_t EN1 = 11;
+uint8_t EN2 = 12;
+uint8_t nFLT1 = 10;
+uint8_t nFLT2 = 13;
 uint8_t faultChannel = 0;
 uint8_t buttonPressed = 0;
-uint8_t Button1 = 2;
-uint8_t Button2 = 8;
-uint8_t Button3 = 3;
-uint8_t Button4 = 9;
-uint8_t keyboardState = 0;
+//uint8_t Button1 = 2;
+//uint8_t Button2 = 8;
+//uint8_t Button3 = 3;
+//uint8_t Button4 = 9;
+//uint8_t keyboardState = 0;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(Button1, INPUT_PULLUP);// 4 Rpi Pico pull_up must be after the attachinterrupt. It's a bug.
-  pinMode(Button2, INPUT_PULLUP);// 4 Rpi Pico pull_up must be after the attachinterrupt. It's a bug.
-  pinMode(Button3, INPUT_PULLUP);// 4 Rpi Pico pull_up must be after the attachinterrupt. It's a bug.
-  pinMode(Button4, INPUT_PULLUP);// 4 Rpi Pico pull_up must be after the attachinterrupt. It's a bug.
+//  pinMode(Button1, INPUT_PULLUP);// 4 Rpi Pico pull_up must be after the attachinterrupt. It's a bug.
+//  pinMode(Button2, INPUT_PULLUP);// 4 Rpi Pico pull_up must be after the attachinterrupt. It's a bug.
+//  pinMode(Button3, INPUT_PULLUP);// 4 Rpi Pico pull_up must be after the attachinterrupt. It's a bug.
+//  pinMode(Button4, INPUT_PULLUP);// 4 Rpi Pico pull_up must be after the attachinterrupt. It's a bug.
   pinMode(EN1, OUTPUT);
   pinMode(EN2, OUTPUT);
   digitalWrite(EN1, HIGH);
   digitalWrite(EN2, HIGH);
-  attachInterrupt(digitalPinToInterrupt(FLT1), Fault1, LOW);
-  attachInterrupt(digitalPinToInterrupt(FLT2), Fault2, LOW);
-  pinMode(FLT1, INPUT_PULLUP);// 4 Rpi Pico pull_up must be after the attachinterrupt. It's a bug.
-  pinMode(FLT2, INPUT_PULLUP);// 4 Rpi Pico pull_up must be after the attachinterrupt. It's a bug.
+  attachInterrupt(digitalPinToInterrupt(nFLT1), Fault1, LOW);
+  attachInterrupt(digitalPinToInterrupt(nFLT2), Fault2, LOW);
+  pinMode(nFLT1, INPUT_PULLUP);// 4 Rpi Pico pull_up must be after the attachinterrupt. It's a bug.
+  pinMode(nFLT2, INPUT_PULLUP);// 4 Rpi Pico pull_up must be after the attachinterrupt. It's a bug.
   
 
   Serial.begin(115200);
@@ -50,7 +50,7 @@ void loop() {
     Serial.println("FLT2");
     faultChannel = 0;
   }
-  keyboardScan();
+//  keyboardScan();
 }
 
 void Fault1()
@@ -62,28 +62,28 @@ void Fault2()
   faultChannel = 2;
 }
 
-uint8_t keyboardScan()
-{
-  if(digitalRead(Button1) == LOW)
-  {
-    keyboardState |= 0b00000001;
-  }
-    if(digitalRead(Button2) == LOW)
-  {
-    keyboardState |= 0b00000010;
-  }
-    if(digitalRead(Button3) == LOW)
-  {
-    keyboardState |= 0b00000100;
-  }
-    if(digitalRead(Button4) == LOW)
-  {
-    keyboardState |= 0b00001000;
-  }
-  delay(150);
-  Serial.println(keyboardState);
-  keyboardState = 0;
-}
+//uint8_t keyboardScan()
+//{
+//  if(digitalRead(Button1) == LOW)
+//  {
+//    keyboardState |= 0b00000001;
+//  }
+//    if(digitalRead(Button2) == LOW)
+//  {
+//    keyboardState |= 0b00000010;
+//  }
+//    if(digitalRead(Button3) == LOW)
+//  {
+//    keyboardState |= 0b00000100;
+//  }
+//    if(digitalRead(Button4) == LOW)
+//  {
+//    keyboardState |= 0b00001000;
+//  }
+//  delay(150);
+//  Serial.println(keyboardState);
+//  keyboardState = 0;
+//}
 
 void waiting_4_command() {
   cmd = "";
